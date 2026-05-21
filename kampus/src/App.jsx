e70a4857f05afc567c699534bdcf485632506383
Syaifur Rohman <syaifurplus@gmail.com>
2026-05-21 15:58:44 +0700

    react router dom (#6)

--- kampus/src/App.jsx
+++ kampus/src/App.jsx
@@ -3,8 +3,8 @@ import Login from "./Pages/Auth/Login/Login";
 import Mahasiswa from "./Pages/Admin/Mahasiswa/Mahasiswa";
 
 const App = () => {
-  // return <Login />
-  return <Mahasiswa />
+  return <Login />
+  // return <Mahasiswa />
 };
 
 export default App;
